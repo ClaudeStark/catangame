84a9079a1cfc0e68a22661401e5d7c6793e5bbf4
Evelinej <122548825+Evelinej@users.noreply.github.com>
2023-12-28 17:16:56 +0100

    Style Homepage
    Startseite mit CSS gemacht.
    Breakpoints: 430px, 1024px (Phone, Tablet, Desktop)
    
    Style Scoped verhinderte, dass ich den Style über die Breakpoints vererben kann, daher wenig hübsch kopiert...

--- src/views/CatanView.vue
+++ src/views/CatanView.vue
@@ -1,5 +1,207 @@
 <template>
-    <h1>CatanView</h1>
+    <div class="header">
+
+        <h1>Catan</h1>
+        <h2>Gemeinsam – aber Digital</h2>
+
+        <button id="btn" @click="goToSession">Starte ein neues Spiel!</button>
+
+    </div>
 </template>
-<script setup></script>
-<style scoped></style>
+
+<script setup>
+import router from '@/router';
+
+function goToSession() {
+    router.push('/session')
+}
+
+</script>
+
+
+<style scoped>
+
+@media (min-width: 1024px) {
+.header {
+    display: flex;
+    height: 100vh;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background-color: #1d4b3c;
+}
+
+h1 {
+    font-size: 12rem;
+    font-weight: 700;
+    margin-bottom: 0rem;
+    font-family: 'Raleway', monospace;
+    text-transform: uppercase;
+    color: #fadb42;
+    letter-spacing: 0.3rem;
+}
+
+h2 {
+    font-size: 2rem;
+    font-weight: 400;
+    margin-top: 0;
+    font-family: 'Work Sans', sans-serif;
+    color: #fadb42;
+    text-transform: uppercase;
+    letter-spacing: 0.48rem;
+    margin-top: -1.5rem;
+}
+
+#btn {
+    font-size: 1.5rem;
+    font-weight: 800;
+    font-family: 'Raleway', sans-serif;
+    color: #1d4b3c;
+    letter-spacing: 0.05rem;
+    padding: 1rem 2rem;
+    background-color: white;
+    border-radius: 100px;
+    margin-top: 5rem;
+    transition: ease-in-out 0.4s;
+}
+
+#btn:hover {
+    background-color: #fadb42;
+    cursor: pointer;
+    transition: ease-in-out 0.4s;
+}
+
+#btn:active {
+    transition: ease-in-out 0.1s;
+    transform: scale(0.98);
+}
+
+}
+
+
+
+/* Media Queries */
+
+/* Smartphones (Portrait) */
+@media (max-width: 430px) {
+    .header {
+    display: flex;
+    height: 100vh;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background-color: #1d4b3c;
+}
+
+h1 {
+    font-size: 5rem;
+    font-weight: 700;
+    margin-bottom: 0rem;
+    font-family: 'Raleway', monospace;
+    text-transform: uppercase;
+    color: #fadb42;
+    letter-spacing: 0.3rem;
+}
+
+h2 {
+    font-size: 1rem;
+    font-weight: 400;
+    margin-top: 0;
+    font-family: 'Work Sans', sans-serif;
+    color: #fadb42;
+    text-transform: uppercase;
+    letter-spacing: 0.12rem;
+    margin-top: -0.5rem;
+}
+
+#btn {
+    font-size: 1rem;
+    font-weight: 800;
+    font-family: 'Raleway', sans-serif;
+    color: #1d4b3c;
+    letter-spacing: 0.05rem;
+    padding: 1rem 2rem;
+    background-color: white;
+    border-radius: 100px;
+    margin-top: 3rem;
+    transition: ease-in-out 0.4s;
+}
+
+#btn:hover {
+    background-color: #fadb42;
+    cursor: pointer;
+    transition: ease-in-out 0.4s;
+}
+
+#btn:active {
+    transition: ease-in-out 0.1s;
+    transform: scale(0.98);
+}
+}
+
+
+/* Tablets (Portrait) */
+
+@media (min-width: 431px) and (max-width: 1023px) {
+    .header {
+    display: flex;
+    height: 100vh;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background-color: #1d4b3c;
+}
+
+h1 {
+    font-size: 8rem;
+    font-weight: 700;
+    margin-bottom: 0rem;
+    font-family: 'Raleway', monospace;
+    text-transform: uppercase;
+    color: #fadb42;
+    letter-spacing: 0.3rem;
+}
+
+h2 {
+    font-size: 1rem;
+    font-weight: 400;
+    margin-top: 0;
+    font-family: 'Work Sans', sans-serif;
+    color: #fadb42;
+    text-transform: uppercase;
+    letter-spacing: 0.53rem;
+    margin-top: -0.75rem;
+}
+
+#btn {
+    font-size: 1.5rem;
+    font-weight: 800;
+    font-family: 'Raleway', sans-serif;
+    color: #1d4b3c;
+    letter-spacing: 0.05rem;
+    padding: 1rem 2rem;
+    background-color: white;
+    border-radius: 100px;
+    margin-top: 5rem;
+    transition: ease-in-out 0.4s;
+}
+
+#btn:hover {
+    background-color: #fadb42;
+    cursor: pointer;
+    transition: ease-in-out 0.4s;
+}
+
+#btn:active {
+    transition: ease-in-out 0.1s;
+    transform: scale(0.98);
+}
+
+} 
+
+
+
+</style>
