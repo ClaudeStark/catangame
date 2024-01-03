d182a2e53c44d76c78ce2a3fd1346167a18b0858
Evelinej <122548825+Evelinej@users.noreply.github.com>
2024-01-03 15:46:14 +0100

    nav style, RulesView

---
+++ src/views/RulesView.vue
@@ -0,0 +1,378 @@
+<template>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Spielablauf</title>
+    </head>
+    <div class="rules">
+        <div class="content">
+            <h1>Spielablauf</h1>
+            <h2>Überblick</h2>
+            <p>
+                Es beginnt der älteste Spieler. Wer an der Reihe ist, hat in der nachstehend genannten Reihenfolge folgende
+                Aktions-Möglichkeiten:</p><br>
+            <ol>
+                <li>Er muss die Rohstofferträge dieses Zuges auswürfeln (das Ergebnis
+                    gilt für alle Spieler).</li>
+                <li>Er kann handeln (➔): Rohstoffe tauschen – auch mit seinen Mitspielern.</li>
+                <li>Er kann bauen (➔): Straßen (➔), Siedlungen (➔), Städte (➔) und/
+                    oder Entwicklungskarten (➔) kaufen.</li>
+            </ol>
+            <br>
+            <p>
+                Zusätzlich: Er kann zu einem beliebigen Zeitpunkt seines Zuges
+                (auch vor dem Würfeln) eine seiner Entwicklungskarten (➔) ausspielen. Danach ist sein linker Nachbar an der
+                Reihe; dieser setzt das
+                Spiel mit Aktion 1 fort.
+            </p>
+            <h2>Detailierter Spielablauf</h2>
+            <h3>Rohstofferträge</h3>
+            <p>Der Spieler am Zug wirft beide Würfel: Die addierte Gesamt-Augenzahl bestimmt die Landfelder, die Erträge
+                erbringen!</p><br>
+            <ul>
+                <li>
+                    Jeder Spieler, der eine Siedlung an einem Feld mit der gewürfelten
+                    Zahl stehen hat, nimmt sich für die Siedlung 1 Rohstoffkarte dieses
+                    Feldes. Hat ein Spieler 2 oder 3 Siedlungen um ein ausgewürfeltes
+                    Landfeld stehen, erhält er für jede Siedlung 1 Rohstoffkarte. Hat ein
+                    Spieler eine Stadt an einem Feld mit der gewürfelten Zahl stehen,
+                    erhält er für die Stadt 2 Rohstoffkarten.
+                </li>
+            </ul>
+
+            <h3>Handel</h3>
+            <p>
+                Danach darf der Spieler Handel treiben, um Rohstoffkarten
+                zu tauschen. Er darf so lange und so oft tauschen, wie es seine
+                Rohstoffkarten (auf der Hand) zulassen. Es gibt zwei Arten
+                von Handel:
+            </p><br>
+            <ol>
+                <li><b>Binnenhandel (Handel mit Mitspielern) (➔):</b>
+                    Der Spieler kann mit allen Spielern Rohstoffkarten tauschen. Er
+                    kann mitteilen, welche Rohstoffe er benötigt und was er bereit ist,
+                    dafür abzugeben. Er kann sich aber auch die Vorschläge seiner
+                    Mitspieler anhören und Gegenangebote machen.<br>
+                    <b>Wichtig:</b><i> Mitspieler dürfen nur mit dem Spieler tauschen, der an
+                        der Reihe ist. Die Mitspieler dürfen nicht untereinander tauschen.</i>
+                </li>
+                <li>
+                    <b>Seehandel (Handel mit der Bank) (➔):</b>
+                    Der Spieler kann auch ohne Spielpartner tauschen.
+                </li>
+            </ol><br>
+            <ul>
+                <li>Er kann immer 4:1 tauschen, indem er 4 gleiche Rohstoffkarten
+                    auf den Stapel zurücklegt und sich dafür 1 Rohstoffkarte seiner
+                    Wahl nimmt.</li>
+                <li>Besitzt er eine Siedlung an einem Hafenstandort (➔), kann er
+                    günstiger tauschen: An einem 3:1-Hafen tauscht man 3 gleiche
+                    Rohstoffkarten in 1 Rohstoffkarte nach Wahl, an einem
+                    Spezialhafen tauscht man 2 Rohstoffkarten des angegebenen
+                    Rohstoffs in 1 Rohstoffkarte nach Wahl.</li>
+            </ul>
+
+            <h3>Bauen</h3>
+            <p>Schließlich kann der Spieler bauen, um seine Rohstofferträge zu erhöhen und um die Anzahl seiner Siegpunkte
+                (➔) zu erhöhen!</p><br>
+
+            <h4>Strasse</h4>
+            <ul>
+                <li>
+                    Um zu bauen, muss er bestimmte Kombinationen von Rohstoffkarten (siehe Karte Baukosten) abgeben und
+                    setzt entsprechend
+                    Straßen, Siedlungen oder Städte aus seinem Vorrat auf das Spielfeld.
+                    Abgegebene Rohstoffkarten werden in die jeweiligen Fächer der
+                    Kartenhalter zurückgelegt.
+                </li>
+                <li>
+                    Straßen werden auf Wegen gebaut. Auf jedem Weg (➔) darf nur
+                    1 Straße gebaut werden.
+                </li>
+                <li>
+                    Eine Straße darf nur an eine Kreuzung angelegt werden, an die eine
+                    eigene Straße, Siedlung oder Stadt grenzt und auf der keine fremde
+                    Siedlung oder Stadt steht.
+                </li>
+                <li>
+                    Sobald ein Spieler einen durchgehenden Straßenzug (Abzweigungen zählen nicht) aus mindestens 5
+                    Einzelstraßen besitzt, die nicht von einer
+                    fremden Siedlung oder Stadt unterbrochen wird,
+                    erhält er die Sonder-Siegpunktkarte Längste
+                    Handelsstraße (➔). Gelingt es einem anderen
+                    Spieler, eine längere Straße als der aktuelle
+                    Besitzer zu bauen, erhält dieser sofort die Sonderkarte. Der Besitz der Längsten Handelsstraße ist
+                    2 Siegpunkte wert.
+                </li>
+            </ul><br>
+            <h4>Siedlung</h4>
+            <ul>
+                <li>
+                    Die Siedlung muss auf einer Kreuzung gebaut werden, zu der
+                    mindestens eine eigene Straße führt. Dabei muss die Abstandsregel
+                    beachtet werden.
+                </li>
+                <li>
+                    <b>Abstandsregel:</b> Eine Siedlung darf nur dann auf einer Kreuzung
+                    (➔) gebaut werden, wenn die drei angrenzenden Kreuzungen
+                    NICHT von Siedlungen oder Städten besetzt sind – egal wem sie
+                    gehören.
+                </li>
+                <li>
+                    Auch für jede neue Siedlung kann der Besitzer Rohstofferträge aus
+                    angrenzenden Landfeldern erhalten: je 1 Rohstoffkarte, wenn die
+                    Zahl des Feldes gewürfelt wird
+                </li>
+                <li>
+                    Jede Siedlung zählt <b>1 Siegpunkt.</b>
+                </li>
+            </ul><br>
+
+            <h4>Stadt</h4>
+            <ul>
+                <li>
+                    Baut der Spieler eine seiner Siedlungen zur Stadt aus, so legt er die
+                    Siedlung zu seinem Vorrat zurück und ersetzt diese durch eine
+                    Stadt.
+                </li>
+                <li>
+                    Für eine Stadt erhält der Besitzer doppelt so viele Rohstofferträge
+                    aus angrenzendenen Landfeldern: je 2 Rohstoffkarten, wenn die
+                    Zahl des Feldes gewürfelt wird.
+                </li>
+                <li>
+                    Jede Stadt zählt <b>2 Siegpunkte.</b>
+                </li>
+            </ul><br>
+
+            <h4>Entwicklungskarte</h4>
+            <ul>
+                <li>
+                    Wer eine Entwicklungskarte kauft, zieht die oberste Entwicklungskarte vom Stapel.
+                </li>
+                <li>
+                    Es gibt drei verschiedene Arten dieser Karten, die alle unterschiedliche Auswirkungen haben:
+                    Ritter (➔); Fortschritt (➔); Siegpunkte (➔).
+                </li>
+                <li>
+                    Gekaufte Entwicklungskarten hält man bis zur Verwendung
+                    geheim.
+                </li>
+            </ul><br>
+
+            <h3>Sonderfälle</h3>
+            <h4>7 gewürfelt</h4>
+            <ul>
+                <li>
+                    Würfelt der Spieler, der an der Reihe ist, eine „7“, so erhält
+                    kein Spieler Rohstofferträge.
+                </li>
+                <li>
+                    Alle Spieler, die mehr als 7 Rohstoffkarten besitzen, wählen
+                    die Hälfte ihrer Rohstoffkarten aus und legen diese zurück
+                    in die jeweiligen Fächer der Kartenhalter. Bei ungeraden
+                    Zahlen wird zu Gunsten des betroffenen Spielers abgerundet
+                    (wer z.B. 9 Karten hat, muss 4 davon abgeben).
+                </li>
+                <li>
+                    Danach muss der Spieler den Räuber versetzen:
+                </li>
+            </ul><br>
+            <ol>
+                <li>
+                    Der Spieler muss den Räuber auf ein anderes Landfeld
+                    versetzen.
+                </li>
+                <li>
+                    Dann raubt er einem Mitspieler, der eine Siedlung oder
+                    Stadt an dem Landfeld besitzt, auf welches der Räuber
+                    gestellt wurde, 1 Rohstoffkarte. Der Spieler, der beraubt
+                    wird, hält dabei seine Rohstoffkarten verdeckt in der Hand.
+                </li>
+                <li>Danach setzt der Spieler seinen Zug mit der Handelsphase fort.</li>
+            </ol><br>
+            <p><b>Wichtig:</b><i> Wird das Feld gewürfelt, auf dem der Räuber steht, erhalten
+                    die Besitzer angrenzender Siedlungen und Städte KEINE Rohstofferträge.</i></p><br>
+            <h4>Entwicklungskarte spielen</h4>
+            <p>
+                Ein Spieler, der an der Reihe ist, darf zu einem beliebigen Zeitpunkt
+                seines Zuges 1 Entwicklungskarte ausspielen – auch vor dem
+                Würfeln. Das darf aber NICHT eine Karte sein, die er in diesem
+                Zug gekauft hat.
+            </p>
+            <h5>Ritter</h5>
+            <ul>
+                <li>
+                    Wer eine Ritterkarte ausspielt, versetzt den Räuber.
+                    Siehe Punkte 1. + 2. oben.
+                </li>
+                <li>
+                    Ausgespielte Ritterkarten bleiben offen vor ihrem
+                    Besitzer liegen.
+                </li>
+                <li>
+                    Wer zuerst 3 Ritterkarten offen vor sich liegen hat, erhält die
+                    Sonderkarte Größte Rittermacht, die 2 Siegpunkte wert ist.
+                </li>
+                <li>
+                    Sobald ein anderer Spieler mehr Ritterkarten
+                    offen vor sich liegen hat als der aktuelle Besitzer
+                    der Größten Rittermacht, so erhält er sofort
+                    die Sonderkarte und die damit verbundenen
+                    2 Siegpunkte.
+                </li>
+            </ul><br>
+            <h5>Fortschritt</h5>
+            <p>Wer eine solche Karte spielt, führt die Anweisung des Textes aus. Danach wird die
+                Karte aus dem Spiel entfernt.</p><br>
+            <h5>Siegpunkt</h5>
+            <p>Karten mit Siegpunkten werden grundsätzlich geheim gehalten. Sie werden erst aufgedeckt, wenn ein Spieler
+                insgesamt 10 Siegpunkte erreicht hat.</p><br>
+            <h3>Spielende</h3>
+            <p>Das Spiel endet mit dem Zug, in dem ein Spieler <b>10 oder mehr Siegpunkte</b> erreicht. Um zu gewinnen, muss
+                ein
+                Spieler an der Reihe sein und in seinem Zug mindestens <b>10 Siegpunkte</b> erreichen/besitzen.</p><br>
+
+            <button id="btn" @click="goToSession">Los geht's!</button><br>
+            <span></span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import router from '@/router';
+
+function goToSession() {
+    router.push('/session')
+}
+
+</script>
+
+<style scoped>
+.rules {
+    display: flex;
+    max-height: 88vh;
+    width: 100%;
+    padding: 1.5em;
+    align-items: center;
+    flex-direction: column;
+    background-color: #1d4b3c;
+    overflow-y: auto;
+    padding-right: 17px;
+    box-sizing: content-box;
+}
+
+.content {
+    width: 55%;
+}
+
+h1 {
+    font-weight: 700;
+    margin-bottom: 0rem;
+    font-family: 'Raleway', sans-serif;
+    text-transform: uppercase;
+    color: #fadb42;
+    letter-spacing: 0.05em;
+    margin-top: 0;
+}
+
+h2 {
+    font-weight: 500;
+    font-family: 'Work Sans', sans-serif;
+    color: #fadb42;
+    text-transform: uppercase;
+    margin-top: 2em;
+}
+
+h3 {
+    color: #fadb42;
+    font-weight: 500;
+    letter-spacing: 0.08em;
+    margin-top: 1.5em;
+    margin-bottom: 0.2em;
+    text-align: left;
+    text-transform: uppercase;
+}
+
+h4 {
+    color: #fff;
+    text-align: left;
+    margin-top: 0.8em;
+    margin-bottom: 0.2em;
+}
+
+h5 {
+    color: #fadb42;
+    text-align: left;
+    margin-top: 0.8em;
+    margin-bottom: 0.2em;
+    text-transform: uppercase;
+    letter-spacing: 0.05em;
+}
+
+p {
+    color: #fff;
+    text-align: left;
+}
+
+ol {
+    color: #fff;
+    text-align: left;
+    padding-left: 2em;
+}
+
+ul {
+    color: #fff;
+    text-align: left;
+    padding-left: 2em;
+}
+
+#btn {
+    font-weight: 800;
+    font-family: 'Raleway', sans-serif;
+    color: #1d4b3c;
+    letter-spacing: 0.05rem;
+    padding: 1rem 2rem;
+    background-color: white;
+    border-radius: 100px;
+    margin-top: 1rem;
+    transition: ease-in-out 0.4s;
+}
+
+#btn:hover {
+    background-color: #fadb42;
+    cursor: pointer;
+    transition: ease-in-out 0.4s;
+}
+
+#btn:active {
+    transition: ease-in-out 0.1s;
+    transform: scale(0.98);
+}
+
+/* Media Queries */
+
+/* Smartphones (Portrait) */
+@media (max-width: 430px) {
+    .content {
+        width: 80%;
+    }
+
+    .rules {
+        max-height: 87vh;
+        padding-right: 0;
+        box-sizing: border-box;
+    }
+}
+
+/* Tablets (Portrait) */
+@media (min-width: 431px) and (max-width: 1023px) {
+    .content {
+        width: 75%;
+    }
+
+    .rules {
+        max-height: 90vh;
+    }
+}
+</style>
